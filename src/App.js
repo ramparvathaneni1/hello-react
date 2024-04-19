385f829d53969ee6359447c4ad82071c93e4569b
Ram P <ramsuhas.parvathaneni@prudential.com>
2024-04-19 12:26:17 -0400

    pushing local changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+function Hello(props) {
+
+  const [moodPoints, setMood] = useState(1);
+
+  const onClickHappy = () => {
+    return moodPoints < 10 ? setMood(moodPoints + 1) : setMood(0);
+  };
+
+  const onClickSad = () => {
+    return moodPoints > 0 ? setMood(moodPoints - 1) : setMood(10);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Waaazzaaap!! {props.name}</h1>
+      <h2>Age = {props.age}</h2>
+      <h3>It is time for some tea!</h3>
+
+      <h4>Vehicle = {props.car.name}</h4>
+
+      <h4>Likes = {props.likes.join(", ")}</h4>
+
+      <p>Happiness Level = {moodPoints}</p>
+      <button onClick={onClickHappy}> Happy </button>
+      &nbsp;
+      <button onClick={onClickSad}> Sad </button>
+
     </div>
   );
 }
 
-export default App;
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const person = [{
+  name: "Dominic Toretto",
+  age: 40,
+  car: {
+    name: "Charger"
+  },
+  likes: ["Birds", "Animals", "Dinosaurs"]
+}, {
+  name: "Carl Sagan",
+  age: 62,
+  car: {
+    name: "Spaceship"
+  },
+  likes: ["Stars", "Galaxies", "Astronomy"]
+}];
 root.render(
   <React.StrictMode>
-    <App />
+    <App name={person[0].name} age={person[0].age} car={person[0].car} likes={person[0].likes} />
+    <App name={person[1].name} age={person[1].age} car={person[1].car} likes={person[1].likes} />
   </React.StrictMode>
 );
 
